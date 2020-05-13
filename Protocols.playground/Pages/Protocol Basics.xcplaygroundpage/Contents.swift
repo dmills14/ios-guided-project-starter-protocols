d53452e066ebcb5231e0f71950483de3c41ef2d1
dmills14 <drew-miller@lambdastudents.com>
2020-05-13 09:51:20 -0700

    Added FullyNamed protocol

--- Protocols.playground/Pages/Protocol Basics.xcplaygroundpage/Contents.swift
+++ Protocols.playground/Pages/Protocol Basics.xcplaygroundpage/Contents.swift
@@ -6,9 +6,33 @@ import Foundation
 //: "... a blueprint of methods, properties, and other requirements that suit a particular task or piece of functionality. The protocol can then be adopted by a class, structure, or enumeration to provide an actual implementation of those requirements. Any type that satisfies the requirements of a protocol is said to conform to that protocol."
 //:
 //: The below example shows a protocol that requires conforming types have a particular property defined.
+//Anyone using this protocol must have a full name
+protocol FullyNamed {
+    var fullName: String { get }
+}
 
+struct Person: FullyNamed {
+    var fullName: String
+    
+}
 
-
+class Starship: FullyNamed {
+    var prefix: String?
+    var name: String
+    
+    init(prefix: String?, name: String) {
+        self.prefix = prefix
+        self.name = name
+    }
+    
+    var fullName: String {
+        if let unwrappedPrefix = prefix {
+            return "\(unwrappedPrefix) " + "\(name)"
+        } else {
+            return name
+        }
+    }
+}
 //: Protocols can also require that conforming types implement certain methods.
 
 
